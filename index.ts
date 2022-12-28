f2675f8cbc49feb6e4efdad9704c5e098914d9e0
arturo <ahpareja@gmail.com>
2022-12-28 15:34:51 -0500

    Hello params route

--- index.ts
+++ index.ts
@@ -15,10 +15,40 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to App Express + TS + Swagger + Mongoose');
 });
 
-//Define the second Route of the app
 app.get('/hello', (req: Request, res: Response) => {
-  //Send hello world
-  res.send('Hello World');
+  //Send hello + name
+  const responseData = {
+    "data":{
+      "message": `Hello, anonimo.`
+    }
+  };
+  res.json(responseData);
+});
+
+//Define the second Route of the app
+app.get('/hello/:name', (req: Request, res: Response) => {
+  //Send hello + name
+  const name = req.params.name;
+  
+  const responseData = {
+    "data":{
+      "message": `Hello, ${name}.`
+    }
+  };
+  res.json(responseData);
+});
+
+//Define the third Route of the app
+app.get('/bye', (req: Request, res: Response) => {
+  //Send Goodbye, world
+  const responseData = {
+    "data":{
+      "message": "Goodbye, world"
+    }
+  };
+
+  res.json(responseData)
+
 });
 
 //Execute App and Listen Request port
