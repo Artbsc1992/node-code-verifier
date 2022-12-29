6bc7c3b967f469277eeb83ab1f93df38e501f666
arturo <ahpareja@gmail.com>
2022-12-28 23:12:08 -0500

    Server config done, cors & helmet basic config & rootRouter ready

--- index.ts
+++ index.ts
@@ -1,55 +1,21 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import server from './src/server';
+import { LogError, LogSuccess } from './src/utils/logger';
 
 //configuration the .env file
 dotenv.config();
 
 // Create Express App
 
-const app: Express = express();
 const port: string | number = process.env.PORT || 8000;
 
-//Define the first Route of the app
-app.get('/', (req: Request, res: Response) => {
-  //Send hello world
-  res.send('Welcome to App Express + TS + Swagger + Mongoose');
+// * Execute server
+server.listen(port, () => {
+  LogSuccess(`[SERVER ON]: Running in http://localhost:${port}/api`)
 });
 
-app.get('/hello', (req: Request, res: Response) => {
-  //Send hello + name
-  const responseData = {
-    "data":{
-      "message": `Hello, anonimo.`
-    }
-  };
-  res.json(responseData);
+// * control Server Error
+
+server.on('error', (error) => {
+  LogError(`[SERVER ERROR]: ${error}`)
 });
-
-//Define the second Route of the app
-app.get('/hello/:name', (req: Request, res: Response) => {
-  //Send hello + name
-  const name = req.params.name;
-  
-  const responseData = {
-    "data":{
-      "message": `Hello, ${name}.`
-    }
-  };
-  res.json(responseData);
-});
-
-//Define the third Route of the app
-app.get('/bye', (req: Request, res: Response) => {
-  //Send Goodbye, world
-  const responseData = {
-    "data":{
-      "message": "Goodbye, world"
-    }
-  };
-
-  res.json(responseData)
-
-});
-
-//Execute App and Listen Request port
-app.listen(port, () => console.log(`EXPRESS SERVER: Runing at http://localhost:${port}`));
